842981c2987f767cc0eea53485bd7111897770c0
Luis Carlin <luiscarlin@gmail.com>
2023-10-02 09:46:17 -0400

    casting and converting

---
+++ casting-and-converting.rb
@@ -0,0 +1,13 @@
+
+# convert decimal to integer
+puts  3.14.to_i
+
+# convert to float
+puts  3.to_f
+
+# convert decimal to string
+puts 3.0.to_s
+
+# convert string to integer and perform operations
+puts 100 + "50".to_i
+puts  100 + "50.99".to_f
