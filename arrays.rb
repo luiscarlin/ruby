2509e3f6b6df1c77f7a9f6631699a619add0c54d
Luis Carlin <luiscarlin@gmail.com>
2023-10-03 09:04:14 -0400

    arrays

---
+++ arrays.rb
@@ -0,0 +1,18 @@
+lucky_numbers = [4, 8, "fifteen", 16, 23, 42.0]
+#       indexes  0  1       2      3   4   5
+
+lucky_numbers[0] = 90
+puts lucky_numbers[0]
+puts lucky_numbers[1]
+puts lucky_numbers[-1]
+
+puts "\n\n"
+# array from index 2, length 3
+puts lucky_numbers[2,3]
+puts "\n\n"
+
+# array from index 2 to inxdex 4 (including)
+puts lucky_numbers[2..4]
+puts "\n\n"
+
+puts lucky_numbers.length
